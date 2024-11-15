6803603d3579f5075de8d32495ae941a3c455285
Moses Dare Aleriwon <aleriwonmoses@gmail.com>
2024-11-15 23:14:24 +0100

    first commit of project

---
+++ components/Footer.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+const d = new Date();
+let year = d.getFullYear();
+
+export function Footer (){
+    return(
+        <footer >
+            <ul className="flex lg:justify-between bg-white py-4 md:py-6 px-2 md:px-8 lg:px-16 lg:px-16">
+                <li className="text-xs text-gray-600">&copy; {year} Early-Care Limited. All rights reserved.</li>
+                <li className="text-xs text-gray-600"><Link href="#">Contact</Link></li>
+                <li className="text-xs text-gray-600"><Link href="#">Privacy Policy | Terms of Condition</Link></li>
+            </ul>
+
+        </footer>
+    )
+}
